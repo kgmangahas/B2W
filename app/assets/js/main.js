6108be8aed91c74e4e3ac571a8ca8443e98f9a80
Kate <kateline.mangahas@gmail.com>
2017-07-06 15:54:22 +0800

    Completed blog articles.

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,25 +1,27 @@
-	$(function() {
+$(function() {
+	
+	// Cache the Window object
+	var $window = $(window);
+	
+	// Parallax Backgrounds
+	// Tutorial: http://code.tutsplus.com/tutorials/a-simple-parallax-scrolling-technique--net-27641
+	
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
 		
-		//cache the window object
-		var $window = $(window);
+		$(window).scroll(function() {
 		
-		//parallax background effect
-		$('section[data-type="background"]').each(function() {
-			var $bgobj = $(this);
-			$(window).scroll(function() {
-				
-				//scroll the background at var speed
-				// the yPos is a negative value because we're scrolling it up
-				
-				var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-				
-				//put together our final background position
-				var coords = '50%' + yPos + 'px';
-				
-				// Move the background
-				$bgobj.css({ bacgroundPosition: coords});
-			});
-		});
-		
-		
+			// Scroll the background at var speed
+			// the yPos is a negative value because we're scrolling it UP!								
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+			
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+			
+		}); // end window scroll
 	});
+	
+});
